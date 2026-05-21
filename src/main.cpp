1e52a9ddde473aeabca4b503c922960a51937d76
AstrodiR2 <astrodir2@gmail.com>
2026-05-21 19:17:42 +0300

    фикс: явные имена классов в modify

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,10 @@
 
 using namespace geode::prelude;
 
-$modify(PlayLayer) {
+class $modify(MFPlayLayer, PlayLayer) {
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
         if (!PlayLayer::init(level, useReplay, dontCreateObjects))
             return false;
-
         ReplaySystem::get().onLevelStart();
         return true;
     }
@@ -25,7 +24,7 @@ $modify(PlayLayer) {
     }
 };
 
-$modify(GJBaseGameLayer) {
+class $modify(MFGameLayer, GJBaseGameLayer) {
     void handleButton(bool down, int button, bool isPlayer1) {
         ReplaySystem::get().handleButton(down, button, isPlayer1);
         GJBaseGameLayer::handleButton(down, button, isPlayer1);
